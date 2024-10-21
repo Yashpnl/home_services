bea880e2faeb5efcf9ad5c1565e303222309fad1
NextGenyash <yash@saurabhinfosys.com>
2024-10-21 17:30:40 +0530

    create Header ,Footer Components

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image"
+import logo from '@/assets/logo.png'
+import silogo from '@/assets/si_logo.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="container flex items-center justify-between py-10">
+      <div>
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          quality={100}
+          alt="homeservices"
+        />
+      </div>
+
+      <div>
+        <Image
+          src={silogo}
+          alt="saurabh infosys"
+        />
+      </div>
+    </footer>
   )
 }
 
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,61 @@
+"use client"
+import Image from "next/image"
+import logo from '@/assets/logo.png'
+import { TbLogin2 } from "react-icons/tb";
+import { RiShoppingBag4Line } from "react-icons/ri";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { useState } from "react";
+import Modal from "../modal/Modal";
+import DropDownMenu from "../modal/DropDownMenu";
 
 const Header = () => {
+
+  const [isLoggedin, setisLoggedin] = useState(true)
+  const [showModal, setShowModal] = useState(false)
+
+  const handleModal = () => {
+    setShowModal(!showModal)
+  }
+
   return (
-    <div>Header</div>
+    <>
+      <header className="container bg-white flex items-center justify-between py-10 mb-7">
+        <div>
+          <Image
+            src={logo}
+            alt="homeservices"
+            width={100}
+            height={100}
+            quality={100}
+          />
+        </div>
+
+        <div className="flex items-center gap-10">
+          <div className="flex items-center gap-4 cursor-pointer">
+            <RiShoppingBag4Line className="text-secondary size-6" />
+
+            <p className="text-xl">Order</p>
+          </div>
+
+          {isLoggedin ? <div className="flex items-center gap-1 cursor-pointer" onClick={handleModal}>
+            <p className="text-xl">Eva John</p>
+            <IoMdArrowDropdown className="text-secondary size-10" />
+          </div>
+            :
+            <div className="flex items-center gap-4 cursor-pointer">
+              <TbLogin2 className="text-secondary size-6" />
+              <p className="text-xl">Login</p>
+            </div>
+          }
+        </div>
+      </header>
+
+      {showModal &&
+        <Modal onClose={handleModal}>
+          <DropDownMenu />
+        </Modal>
+      }
+    </>
   )
 }
 
