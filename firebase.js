a2734920ec5beee6a6c3903dd145c1941dfaf928
NextGenyash <yash@saurabhinfosys.com>
2024-10-25 16:29:49 +0530

    firebase update

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, signInWithPopup, RecaptchaVerifier } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    RecaptchaVerifier,
+    signInWithPhoneNumber,
+} from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCtn-DjhBb2oHWeK_MWV25gjbVL_UwBG1k",
     authDomain: "home-services-e02b6.firebaseapp.com",
@@ -17,13 +18,42 @@ const firebaseConfig = {
     measurementId: "G-R9HYTSSTF1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+
 export { auth, RecaptchaVerifier };
 
+/**
+ * Function to send OTP to a specified phone number using Firebase Authentication.
+ * @param {string} phoneNumber - The phone number to which the OTP will be sent.
+ * @param {HTMLDivElement} recaptchaContainer - The container for reCAPTCHA to be attached.
+ * @returns {Promise} - A promise that resolves with the confirmation result containing verificationId.
+ */
+export const sendOtpToPhone = (phoneNumber, recaptchaContainer) => {
+    return new Promise((resolve, reject) => {
+        const recaptchaVerifier = new RecaptchaVerifier(
+            recaptchaContainer,
+            { size: 'invisible' }, // Use 'invisible' for seamless user experience
+            auth
+        );
+
+        signInWithPhoneNumber(auth, phoneNumber, recaptchaVerifier)
+            .then((confirmationResult) => {
+                resolve(confirmationResult);
+            })
+            .catch((error) => {
+                console.error("Error sending OTP:", error);
+                reject(error);
+            });
+    });
+};
+
+/**
+ * Function to initiate Google sign-in using Firebase Authentication.
+ * @returns {Promise} - A promise that resolves with the user's credentials upon successful sign-in.
+ */
 export const signInWithGoogle = () => {
     return signInWithPopup(auth, googleProvider);
 };
