68373cf428cb50edd63e1f452fd1ca7d7ea7e704
NextGenyash <yash@saurabhinfosys.com>
2024-10-25 10:48:16 +0530

    change tabbar fun for reuseable component

--- app/pageContain/Order/components/Tabbar.tsx
+++ app/pageContain/Order/components/Tabbar.tsx
@@ -1,40 +1,52 @@
-"use client"
-import { useState } from 'react';
-import CurrentOrderSection from '../sections/CurrentOrderSection';
-import HistorySection from '../sections/HistorySection';
+const Tabbar = ({ firstTab, secondTab, activeTab, setActiveTab }: { firstTab: string, secondTab?: string | undefined, activeTab: any, setActiveTab: any }) => {
 
-const Tabbar = () => {
-
-    const [activeTab, setActiveTab] = useState("CurrentOrder");
     const handleTabClick = (tab: string) => {
         setActiveTab(tab);
     };
+
     return (
         <>
             <div className="w-[90%] mx-auto bg-white shadow-[0px_3.84px_15.35px_0px_#D4E0EB] flex flex-col">
-                <div className='flex items-center text-center gap-24 p-5 px-16'>
-                    <h2 className={`text-primary cursor-pointer ${activeTab ? "font-bold" : "font-normal"}`}
-                        onClick={() => handleTabClick("CurrentOrder")}>
-                        Current Order</h2>
-                    <h2 className={`text-primary cursor-pointer ${activeTab ? "font-bold" : "font-normal"}`}
-                        onClick={() => handleTabClick("History")}>
-                        History</h2>
-                </div>
+                <div className='flex items-center text-center justify-around sm:justify-start sm:gap-5 sm:px-16'>
+                    <h2 className={`text-primary cursor-pointer sm:text-xl flex flex-col  items-center justify-center ${activeTab === firstTab ? "font-bold" : "font-normal"}`}
+                        onClick={() => handleTabClick(firstTab)}>
+                        <span className="pt-5">{firstTab}</span>
 
-                <div className="px-16 flex gap-20">
-                    <div className={`w-24 h-1  ${activeTab === "CurrentOrder" ? "bg-secondary" : "bg-transparent"}`} />
-                    <div className={`w-24 h-1  ${activeTab === "History" ? "bg-secondary" : "bg-transparent"}`} />
+
+                        {activeTab === firstTab ? (
+                            <svg className="w-1/2 sm:w-full h-[35px]" viewBox="0 0 221 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M182.246 35H35.6114L1.52588e-05 0H221L182.246 35Z" fill="url(#paint0_linear_219_4895)" fill-opacity="0.29" />
+                                <path d="M182.246 35H35.611C35.611 32.2386 37.8496 30 40.611 30H177.246C180.008 30 182.246 32.2386 182.246 35Z" fill="#F9AA58" />
+                                <defs>
+                                    <linearGradient id="paint0_linear_219_4895" x1="111.024" y1="35" x2="111.024" y2="-9.17431e-07" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#F9AA58" />
+                                        <stop offset="1" stop-color="white" stop-opacity="0" />
+                                    </linearGradient>
+                                </defs>
+                            </svg>
+                        ) : <span className="w-1/2 sm:w-[221px] h-[35px]" />}
+                    </h2>
+                    <h2 className={`text-primary cursor-pointer sm:text-xl flex flex-col  items-center justify-center ${activeTab === secondTab ? "font-bold" : "font-normal"}`}
+                        onClick={() => handleTabClick(secondTab)}>
+                        <span className="pt-5">{secondTab}</span>
+
+                        {activeTab === secondTab ? (
+                            <svg className="w-1/2 sm:w-full h-[35px]" viewBox="0 0 221 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M182.246 35H35.6114L1.52588e-05 0H221L182.246 35Z" fill="url(#paint0_linear_219_4895)" fill-opacity="0.29" />
+                                <path d="M182.246 35H35.611C35.611 32.2386 37.8496 30 40.611 30H177.246C180.008 30 182.246 32.2386 182.246 35Z" fill="#F9AA58" />
+                                <defs>
+                                    <linearGradient id="paint0_linear_219_4895" x1="111.024" y1="35" x2="111.024" y2="-9.17431e-07" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#F9AA58" />
+                                        <stop offset="1" stop-color="white" stop-opacity="0" />
+                                    </linearGradient>
+                                </defs>
+                            </svg>
+                        ) : <span className="w-1/2 sm:w-[221px] h-[35px]" />}
+                    </h2>
                 </div>
             </div>
-
-            {
-                activeTab === "CurrentOrder" ?
-                    <CurrentOrderSection />
-                    :
-                    <HistorySection />
-            }
         </>
     )
 }
 
-export default Tabbar
+export default Tabbar;
--- app/pageContain/Order/index.tsx
+++ app/pageContain/Order/index.tsx
@@ -1,12 +1,25 @@
+"use client"
 import Footer from "@/components/footer/Footer"
 import Header from "@/components/header/Header"
 import Tabbar from "./components/Tabbar"
+import { useState } from "react"
+import CurrentOrderSection from "./sections/CurrentOrderSection"
+import HistorySection from "./sections/HistorySection"
 
 const Order = () => {
+
+    const [activeTab, setActiveTab] = useState("Current Order");
+
     return (
         <>
-            <Header /> 
-            <Tabbar /> 
+            <Header />
+            <Tabbar firstTab="Current Order" secondTab="History" activeTab={activeTab} setActiveTab={setActiveTab} />
+            {
+                activeTab === "Current Order" ?
+                    <CurrentOrderSection />
+                    :
+                    <HistorySection />
+            }
             <Footer />
         </>
     )
