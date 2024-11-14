3e502b5e576c230612327676966ff867756f447b
NextGenyash <yash@saurabhinfosys.com>
2024-11-14 12:16:14 +0530

    remove result of search value

--- app/pageContain/Home/sections/popularSection/PopularSection.tsx
+++ app/pageContain/Home/sections/popularSection/PopularSection.tsx
@@ -49,31 +49,29 @@ const PopularSection = () => {
 
     return (
         <>
-            {results?.length > 0 ? '' :
-                <section className="w-[90%] mx-auto mt-20">
-                    <div className="flex items-center justify-between">
-                        <h3 className="text-xl font-semibold">Popular Services</h3>
-                        <button
-                            className="text-xl text-primary"
-                            onClick={handleToggleViewAll}
-                        >
-                            {allServices?.length >= initialServicesToShow ?
-                                `${showAll ? 'View less' : 'View all'}` : ''
-                            }
-                        </button>
-                    </div>
+            <section className="w-[90%] mx-auto mt-20">
+                <div className="flex items-center justify-between">
+                    <h3 className="text-xl font-semibold">Popular Services</h3>
+                    <button
+                        className="text-xl text-primary"
+                        onClick={handleToggleViewAll}
+                    >
+                        {allServices?.length >= initialServicesToShow ?
+                            `${showAll ? 'View less' : 'View all'}` : ''
+                        }
+                    </button>
+                </div>
 
-                    <div className="grid sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-5 pt-5">
-                        {allServices?.slice(0, showAll ? allServices.length : initialServicesToShow).map((services, serviceskey) => (
-                            <ServiceCard
-                                key={serviceskey}
-                                serviceicon={services?.category_url}
-                                servicename={services?.category_name}
-                            />
-                        ))}
-                    </div>
-                </section>
-            }
+                <div className="grid sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-8 gap-5 pt-5">
+                    {allServices?.slice(0, showAll ? allServices.length : initialServicesToShow).map((services, serviceskey) => (
+                        <ServiceCard
+                            key={serviceskey}
+                            serviceicon={services?.category_url}
+                            servicename={services?.category_name}
+                        />
+                    ))}
+                </div>
+            </section>
         </>
     );
 };
